0b7947668e34769b156203356ea7a1d90ae48302
abiy111111 <muh.abiyraihansyah@gmail.com>
2025-01-08 11:17:00 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/domalapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.domalapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,260 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+    _controller.forward();
+
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const NextScreen()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              // Logo animation
+              FadeTransition(
+                opacity: _animation,
+                child: ScaleTransition(
+                  scale: _animation,
+                  child: Image.asset(
+                    'assets/donately.png', // Logo utama
+                    width: 221, // Ukuran diperbesar dari 200 ke 300
+                    height: 158, // Ukuran diperbesar dari 200 ke 300
+                  ),
+                ),
+              ),
+              const Spacer(),
+             
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Text(
+                  'simetrya group',
+                  style:GoogleFonts.dmSerifText(
+                   textStyle:  TextStyle(
+                    color: Colors.green[800],
+                    fontSize: 16,
+                     fontWeight: FontWeight.w500,
+                     shadows: [
+                    Shadow(
+                     offset: const Offset(0, 4), // y = 4
+                     blurRadius: 4,              // blur = 4
+                     color: Colors.black.withOpacity(0.25), // standar opacity shadow
+                     ),
+                    ],
+                    )
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Halaman berikutnya (ganti sesuai kebutuhan)
+class NextScreen extends StatefulWidget {
+  const NextScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NextScreen> createState() => _NextScreenState();
+}
+
+class _NextScreenState extends State<NextScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _slideAnimation;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    _slideAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end:1.4,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    _opacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.5, 1.0, curve: Curves.easeIn),
+    ));
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                AnimatedBuilder(
+                  animation: _controller,
+                  builder: (context, child) {
+                    return Transform.translate(
+                      offset: Offset(
+                        Tween<double>(
+                          begin: 0,
+                          end: -100,
+                        ).evaluate(_slideAnimation),
+                        0,
+                      ),
+                      child: Transform.scale(
+                        scale: _scaleAnimation.value,
+                        child: Image.asset(
+                          'assets/donately.png',
+                          width: 20, // Ukuran diperbesar dari 200 ke 300
+                          height: 10, // Ukuran diperbesar dari 200 ke 300
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                Transform.translate(
+                  offset: const Offset(80, 0),
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Domal',
+                          style: GoogleFonts.rozhaOne(
+                            color: Color(0xff2F614D),
+                            fontSize: 48,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        Text(
+                          'membantu sesama manusia',
+                          style: GoogleFonts.poppins(
+                            color: Color(0xff2F614D),
+                            fontSize: 10,
+                            fontWeight: FontWeight.w400
+                          )
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: Text(
+                'Simetrya Group',
+                style: GoogleFonts.dmSerifText(
+                  textStyle: TextStyle(
+                    color: Colors.green[800],
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    shadows: [
+                      Shadow(
+                        offset: const Offset(0, 4), // y = 4
+                        blurRadius: 4,              // blur = 4
+                        color: Colors.black.withOpacity(0.25), // standar opacity shadow
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
